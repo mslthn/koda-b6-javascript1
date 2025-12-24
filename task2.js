96662ebff7fe1ab9e3a8241e81d22aa95cf67e60
Muhammad Sulthon Imam Puro <75915809+mslthn@users.noreply.github.com>
2025-12-24 14:04:35 +0700

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,20 @@
+const we = {
+    are: {
+        the: {
+            best: "Koda"
+        }
+    }
+}
+
+const hello = {
+    world : "Hello World"
+}
+
+const obj = {
+    str : [0,1,2,[0,[0,1,2]]]
+}
+const my = []
+
+console.log(we.are.the.best)
+console.log(hello.world)
+console.log(obj.str[3][1][2])
