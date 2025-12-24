13e5f13214889e702f24531cf252e45546a86a0f
Muhammad Sulthon Imam Puro <75915809+mslthn@users.noreply.github.com>
2025-12-24 14:26:24 +0700

    Add files via upload

--- task2.js
+++ task2.js
@@ -11,10 +11,20 @@ const hello = {
 }
 
 const obj = {
-    str : [0,1,2,[0,[0,1,2]]]
+    str : [0,1,2,[0,[0,1,{man:[{tech:{academy : "Tech Academy"}}] }
+    ]]]
 }
-const my = []
+
+const my = [{favourite : [0,1,2, {fruit : {is : "Apple"}}]
+}]
+
+const num = {
+    first : [0,30],
+    second : [0,1,2]
+}
 
 console.log(we.are.the.best)
 console.log(hello.world)
-console.log(obj.str[3][1][2])
+console.log(obj.str[3][1][2].man[0].tech.academy)
+console.log(my[0].favourite[3].fruit.is)
+console.log(num.first[1]+num.second[2])
