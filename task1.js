cb3415a536603f25f2ce0436cbfa4c3cb7e98170
Muhammad Sulthon Imam Puro <75915809+mslthn@users.noreply.github.com>
2025-12-24 10:51:52 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,8 @@
+let r = 7
+
+const phi = 22/7
+const hitungLuas = phi * r * r
+const hitungKeliling = 2 * phi * r
+
+console.log("Luas Lingkaran: " + hitungLuas)
+console.log("Keliling Lingkaran: " + hitungKeliling)
